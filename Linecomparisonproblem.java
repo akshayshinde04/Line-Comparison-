6bd839c28e15e73e2e5bb72e35e0113816f8aa16
captjack-4 <90202533+captjack-4@users.noreply.github.com>
2021-09-27 08:31:14 +0530

    Update Linecomparisonproblem.java

--- Linecomparisonproblem.java
+++ Linecomparisonproblem.java
@@ -1,17 +1,13 @@
 package lineproblem;
 
   public class Linecomparisonproblem {
-	  
-		
 		double length (int x1,int x2,int y1,int y2)
 		{
 			double length;
 			length =Math.sqrt((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)); 
 			return length;
 		}  
-		public static void main(String[] args) {
-			
-		
+		public static void main(String[] args) {	
    Linecomparisonproblem Object = new Linecomparisonproblem();
    double distance = Object.length(1,2,3,4);
 	System.out.println("Length between two point is :" +distance);
