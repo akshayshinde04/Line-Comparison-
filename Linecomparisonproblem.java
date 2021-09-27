442a02cb037fb2a726790b7f3ea9ce4813198547
captjack-4 <90202533+captjack-4@users.noreply.github.com>
2021-09-27 17:04:17 +0530

    Update Linecomparisonproblem.java

--- Linecomparisonproblem.java
+++ Linecomparisonproblem.java
@@ -12,7 +12,7 @@ package lineproblem;
 	Double line1 = Object.length(7,9,5,6);
 	Double line2 = Object.length(2,8,4,3);
 	System.out.println("line1 and line2 are equal :" +line1.equals(line2));
-	}
+	 }
+       }	
 		
-		
-}
+
