56e1601a773332b1e466045a9bd41d1288785600
captjack-4 <90202533+captjack-4@users.noreply.github.com>
2021-09-27 09:54:04 +0530

    Update Linecomparisonproblem.java

--- Linecomparisonproblem.java
+++ Linecomparisonproblem.java
@@ -1,13 +1,13 @@
 package lineproblem;
 
-  public class Linecomparisonproblem {
+       public class Linecomparisonproblem {
 	  double length (int x1,int x2,int y1, int y2)
 		{
-			double length;
-			length=Math.sqrt((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1));
-			return length;
+          double length;
+          length=Math.sqrt((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1));
+	  return length;
 		}
-public static void main(String[] args) {
+      public static void main(String[] args) {
 	Linecomparisonproblem Object = new Linecomparisonproblem();
 	Double line1 = Object.length(7,9,5,6);
 	Double line2 = Object.length(2,8,4,3);
