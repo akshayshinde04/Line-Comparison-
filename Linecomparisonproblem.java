0bc73b7935fe9e6c701d779294967b631cd64656
captjack-4 <90202533+captjack-4@users.noreply.github.com>
2021-09-27 09:57:16 +0530

    Update Linecomparisonproblem.java

--- Linecomparisonproblem.java
+++ Linecomparisonproblem.java
@@ -11,14 +11,14 @@ package lineproblem;
 	Linecomparisonproblem Object = new Linecomparisonproblem();
 	Double line1 = Object.length(1,6,1,5);
 	Double line2 = Object.length(2,4,2,3);
-	{
+	
 	if(Double.compare(line1,line2) == 0)
 		System.out.println("Both the lines are Equal");
 	else if(Double.compare(line1,line2) < 0)
 		System.out.println("line2 is greater than line 1");
 	else
 		System.out.println("line 1 is greater than line 2 ");
-	}
+	
 	}
 		
 		
